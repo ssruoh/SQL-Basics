9304eb257f51441889ff3c3f9f6e856040546dd2
ssruoh <ssruoh@gmail.com>
2021-01-09 17:57:58 +0000

    Continuing

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,11 +13,11 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor:
+            print(row)
 finally:
     # Close the connection, regardless of whether or not the above was
     #  successful
